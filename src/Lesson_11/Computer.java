c2dcace5106d4f80d50353293aa27d7ce5897d9f
istoric564 <mrdizzy38@gmail.com>
2021-03-14 22:15:04 +0700

    Java для начинающих. 11.1 Принципы ООП. Инкапсуляция

---
+++ src/Lesson_11/Computer.java
@@ -0,0 +1,14 @@
+package Lesson_11;
+
+public class Computer {
+    private Ssd ssd;
+    private Ram ram;
+
+    public Computer(Ssd ssd, Ram ram) {
+        this.ssd = ssd;
+        this.ram = ram;
+    }
+    public void load(){
+        System.out.println("Я загрузился");
+    }
+}
