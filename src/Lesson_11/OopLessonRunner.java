84c4a54db30184693507088d4616aa22aadcc9fa
istoric564 <mrdizzy38@gmail.com>
2021-03-15 18:21:59 +0700

    Java для начинающих. 13.2 Приведение типов. Оператор instanceof

--- src/Lesson_11/OopLessonRunner.java
+++ src/Lesson_11/OopLessonRunner.java
@@ -4,13 +4,16 @@ public class OopLessonRunner {
     public static void main(String[] args) {
         Computer laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
         Computer mobile = new Mobile(new Ssd(125), new Ram(512));
-        loadComputers(laptop,mobile);
+//        loadComputers(laptop,mobile);
         printInformation(new Computer[]{laptop,mobile});
 
     }
     public static void printInformation(Computer[] computers){
         for (Computer computer : computers){
             computer.print();
+            if (computer instanceof Laptop){
+                ((Laptop) computer).open();
+            }
         }
     }
     public static void loadComputers(Computer... computers) {
