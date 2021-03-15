818b5e7d6d9848fb0246e4fcc359d47c1dbc9b66
istoric564 <mrdizzy38@gmail.com>
2021-03-15 18:57:54 +0700

    Java для начинающих. 14.2 Интерфейсы. Нововведения

--- src/Lesson_11/OopLessonPunner3.java
+++ src/Lesson_11/OopLessonPunner3.java
@@ -2,14 +2,23 @@ package Lesson_11;
 
 public class OopLessonPunner3 {
     public static void main(String[] args) {
-        Computer laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
-        Computer mobile = new Mobile(new Ssd(125), new Ram(512));
+        Printable laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
+        Printable mobile = new Mobile(new Ssd(125), new Ram(512));
 
-        print(laptop,mobile);
+        printWithRandom(laptop,mobile);
     }
     public static void print(Printable... objects){
+//        iter
         for (Printable object : objects) {
             object.print();
+            System.out.println();
+        }
+    }
+    public static void printWithRandom(Printable... objects){
+//        iter
+        for (Printable object : objects) {
+            object.printWithRandom();
+            System.out.println();
         }
     }
 }
--- src/Lesson_11/Printable.java
+++ src/Lesson_11/Printable.java
@@ -1,8 +1,18 @@
 package Lesson_11;
 
+import java.util.Random;
+
 public interface Printable {
 //    Модификатор доступа, тип, по умолчанию public static final
         String SOME_VALUE = "Example";
-
+        Random RANDOM = new Random();
+        default void printWithRandom(){
+                System.out.println(RANDOM.nextInt());
+                print();
+        }
         void print();
+
+        private int generateRandom(){
+                return RANDOM.nextInt();
+        }
 }
